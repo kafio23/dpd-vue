18afde39797b6b11e5beb0ef76ed03ff7131b0dd
fquino <dkafio@gmail.com>
2019-02-23 15:36:21 -0500

    login first steps

--- app/app1.js
+++ app/app1.js
@@ -1,18 +1,15 @@
 import Vue from "nativescript-vue";
 
 import Home from "./components/Home";
-import Login from "./components/Login";
-
-Vue.registerElement("Gradient", () => require("nativescript-gradient").Gradient);
 
 new Vue({
 
     template: `
         <Frame>
-            <Login />
+            <Home />
         </Frame>`,
 
     components: {
-        Login
+        Home
     }
 }).$start();
---
+++ app/components/Login.vue
@@ -0,0 +1,80 @@
+<template>
+  <Page>
+    <ActionBar title="App Demo"/>
+      <Gradient direction="to right" colors="#4781FE, #3A53FF, #02556E">
+        <FlexboxLayout flexDirection="column" justifyContent="center"
+        class="all-height">
+          <Label class="logo">
+            <FormattedString>
+              <Span class="fa" text.decode="&#xf135; "/>
+            </FormattedString>
+          </Label>
+          
+          <FlexboxLayout alignItems="center" class="border-bottom">
+            <Image src="~/assets/images/icon_user.png" class="icon-margin" witdh="18"
+            height="18" />
+            <TextField v-model="usernameInput" hint="Usuario"
+            class="form-input"/>
+          </FlexboxLayout>
+
+          <FlexboxLayout alignItems="center" class="border-bottom">
+            <Image src="~/assets/images/icon_password.png" class="icon-margin" witdh="18"
+            height="18" />
+            <TextField v-model="passwordInput" hint="Contraseña" secure="true"
+            class="form-input"/>
+          </FlexboxLayout>
+
+          <Button text="Login" @tap="onButtontap" class="login-button" />
+        </FlexboxLayout>
+      </Gradient>
+  </Page>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      usernameInput: '',
+      passwordInput: '',
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import "../app-variables";
+.border-bottom {
+  border-bottom-width: 1;
+  border-bottom-color: $white;
+}
+.fa {
+  color: $white;
+}
+.form-input {
+  color: $white;
+  placeholder-color: $white;
+}
+.logo {
+  font-size: 60;
+}
+.icon-margin {
+  margin-top:3;
+  margin-right:10;
+}
+.login-button {
+  background-color: #7881FE;
+  color: $white;
+  font-weight: bold;
+  border-radius: 30;
+  padding-top: 15;
+  padding-bottom: 15;
+  text-transform: uppercase;
+  letter-spacing: 0.2;
+}
+.all-height {
+  height: 100%
+}
+</style>
