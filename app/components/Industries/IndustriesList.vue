92d68235388379f60f190f7aa6ce95e5208c7c4f
fquino <dkafio@gmail.com>
2019-03-04 01:35:52 -0500

    ajustes en formatos

--- app/components/Industries/IndustriesList.vue
+++ app/components/Industries/IndustriesList.vue
@@ -1,131 +1,142 @@
 <template>
-	<Page actionBarHidden="true">
-		<StackLayout @loaded="load()">
+  <Page actionBarHidden="true">
+    <StackLayout @loaded="load()">
+      <FlexboxLayout alignItems="center" height="10%" class="action-bar-costume">
+        <GridLayout width="10%" @tap="goBack">
+          <Image src="~/assets/images/icon_left.png" witdh="30" height="30"/>
+        </GridLayout>
+        <NavigationButton
+          width="10%"
+          text="Go back"
+          android.systemIcon="ic_menu_back"
+          @tap="goBack"
+        />
+        <Label width="70%" text="Industrias"></Label>
+        <GridLayout width="20%" @tap="showMenu" class="menu-button-container">
+          <Image src="~/assets/images/icon_menu.png" witdh="30" height="30"/>
+        </GridLayout>
+      </FlexboxLayout>
 
-			<FlexboxLayout alignItems="center" height="10%" class="action-bar-costume">
-				<GridLayout width="10%"  @tap="goBack">
-					<Image src="~/assets/images/icon_left.png" witdh="30" height="30" />
-				</GridLayout>
-				<NavigationButton width="10%" text="Go back" android.systemIcon="ic_menu_back" @tap="goBack" />
-				<Label width="70%" text="Industrias"></Label>
-				<GridLayout width="20%" @tap="showMenu" class='menu-button-container'>
-					<Image src="~/assets/images/icon_menu.png" witdh="30" height="30" />
-				</GridLayout>
-			</FlexboxLayout>
-      
-      <ListView :visibility="isBusy ? 'collapsed' : 'visible'" height="90%" for="industry in industryItems" @itemTap="onItemTap">
+      <ListView
+        :visibility="isBusy ? 'collapsed' : 'visible'"
+        height="90%"
+        for="industry in industryItems"
+        @itemTap="onItemTap"
+      >
         <v-template>
           <WrapLayout class="item-container">
-            <FlexboxLayout flexDirection="column" >
-              <Label :text="industry.name" className="startupName" />
-              <Label :text="industry.num" className="startupType" />
+            <FlexboxLayout flexDirection="column">
+              <Label :text="industry.name" class="startupName"/>
+              <Label :text="industry.num" class="startupType"/>
             </FlexboxLayout>
           </WrapLayout>
         </v-template>
       </ListView>
 
-      <ActivityIndicator :visibility="isBusy ? 'visible' : 'collapsed'" width="100%" height="10%" :busy="isBusy"></ActivityIndicator>
-
-		</StackLayout>
-	</Page>
+      <ActivityIndicator
+        :visibility="isBusy ? 'visible' : 'collapsed'"
+        width="100%"
+        height="10%"
+        :busy="isBusy"
+      ></ActivityIndicator>
+    </StackLayout>
+  </Page>
 </template>
 
 <script>
-
-import Login from '@/components/Login'
-import { mapGetters } from 'vuex'
-import { mapActions } from 'vuex'
-import { industriesList, itemList } from '~/store/getters';
-import IndustryStartups from './IndustryStartups.vue'
+import Login from "@/components/Login";
+import { mapGetters } from "vuex";
+import { mapActions } from "vuex";
+import { industriesList, itemList } from "~/store/getters";
+import IndustryStartups from "./IndustryStartups.vue";
 
 export default {
   computed: {
     ...mapGetters({
-      itemList: 'itemList',
-      isLoading: 'isProcessing'
+      itemList: "itemList",
+      isLoading: "isProcessing"
     }),
 
     startupItems: function() {
-      return this.itemList
+      return this.itemList;
     },
 
     industryItems: function() {
-      let industries = []
-      let industryNames = []
-      let num = 0
+      let industries = [];
+      let industryNames = [];
+      let num = 0;
 
-      function onlyUnique(value, index, self) { 
+      function onlyUnique(value, index, self) {
         return self.indexOf(value) === index;
       }
       this.startupItems.forEach(element => {
-        industries.push({ name: element.industry, num: 0})
-      })
+        industries.push({ name: element.industry, num: 0 });
+      });
 
-      var sortByProperty = function (property) {
-          return function (x, y) {
-              return ((x[property] === y[property]) ? 0 : ((x[property] > y[property]) ? 1 : -1));
-          };
+      var sortByProperty = function(property) {
+        return function(x, y) {
+          return x[property] === y[property]
+            ? 0
+            : x[property] > y[property]
+            ? 1
+            : -1;
+        };
       };
-      industries.sort(sortByProperty('name'));
-      var current = '';
+      industries.sort(sortByProperty("name"));
+      var current = "";
       var cnt = 0;
       for (var i = 0; i < industries.length; i++) {
-          if (industries[i].name != current) {
-              if (cnt > 0) {
-                  console.log(current + ' comes --> ' + cnt + ' times<br>');
-                  industryNames.push({ name: current, num: cnt })
-              }
-              current = industries[i].name;
-              cnt = 1;
-          } else {
-              cnt++;
+        if (industries[i].name != current) {
+          if (cnt > 0) {
+            console.log(current + " comes --> " + cnt + " times<br>");
+            industryNames.push({ name: current, num: cnt });
           }
+          current = industries[i].name;
+          cnt = 1;
+        } else {
+          cnt++;
+        }
       }
       if (cnt > 0) {
-          console.log(current + ' comes --> ' + cnt + ' times');
-          industryNames.push({ name: current, num: cnt })
+        console.log(current + " comes --> " + cnt + " times");
+        industryNames.push({ name: current, num: cnt });
       }
-      return industryNames
-    },
-
+      return industryNames;
+    }
   },
 
   data() {
     return {
-      isBusy: true,
-    }
+      isBusy: true
+    };
   },
 
-	mounted() {
-	},
+  mounted() {},
 
-	methods: {
-    ...mapActions([
-      'loadItems',
-    ]),
+  methods: {
+    ...mapActions(["loadItems"]),
 
     goBack() {
-			this.$navigateBack();
+      this.$navigateBack();
     },
 
     load() {
-      this
-        .loadItems()
+      this.loadItems()
         .then(() => {
-          this.isBusy = false
+          this.isBusy = false;
         })
         .catch(error => {
-          console.error(error)
+          console.error(error);
           alert("An error occurred loading your Startup list.");
-        })
+        });
     },
-    
+
     showMenu() {
       action({
         message: "¿Deseas salir?",
         actions: ["Log Off"],
         cancelButtonText: "Cancelar"
-      }).then((result) => {
+      }).then(result => {
         if (result === "Log Off") {
           this.logout();
         }
@@ -133,25 +144,24 @@ export default {
     },
 
     logout: function() {
-      this.$navigateTo(Login)
+      this.$navigateTo(Login);
     },
 
-    onItemTap({item}) {
-			this.$navigateTo(IndustryStartups, { props: { industry: item } } )
-		},
+    onItemTap({ item }) {
+      this.$navigateTo(IndustryStartups, { props: { industry: item } });
+    }
   }
-}
-
+};
 </script>
 
 <style scoped lang="scss">
-Label.startupName {
+label.startupName {
   font-size: 20px;
   color: #2cb5e8;
   font-weight: bold;
 }
-Label.startupType {
-	font-style: italic;
+label.startupType {
+  font-style: italic;
   font-size: 14px;
   font-weight: bold;
 }
--- app/components/Industries/IndustryStartups.vue
+++ app/components/Industries/IndustryStartups.vue
@@ -1,87 +1,91 @@
 <template>
-	<Page actionBarHidden="true">
-		<StackLayout @loaded="load()">
+  <Page actionBarHidden="true">
+    <StackLayout @loaded="load()">
+      <FlexboxLayout alignItems="center" height="10%" class="action-bar-costume">
+        <GridLayout width="10%" @tap="goBack">
+          <Image src="~/assets/images/icon_left.png" witdh="30" height="30"/>
+        </GridLayout>
+        <Label width="70%" :text="`Industria ${industry.name}`"></Label>
+        <GridLayout width="20%" @tap="showMenu" class="menu-button-container">
+          <Image src="~/assets/images/icon_menu.png" witdh="30" height="30"/>
+        </GridLayout>
+      </FlexboxLayout>
 
-			<FlexboxLayout alignItems="center" height="10%" class="action-bar-costume">
-				<GridLayout width="10%"  @tap="goBack">
-					<Image src="~/assets/images/icon_left.png" witdh="30" height="30" />
-				</GridLayout>
-				<Label width="70%" :text="`Industria ${industry.name}`"></Label>
-				<GridLayout width="20%" @tap="showMenu" class='menu-button-container'>
-					<Image src="~/assets/images/icon_menu.png" witdh="30" height="30" />
-				</GridLayout>
-			</FlexboxLayout>
-
-      <ListView :visibility="isBusy ? 'collapsed' : 'visible'" height="90%" for="startup in startups" @itemTap="onItemTap">
+      <ListView
+        :visibility="isBusy ? 'collapsed' : 'visible'"
+        height="90%"
+        for="startup in startups"
+        @itemTap="onItemTap"
+      >
         <v-template>
-          <StartupItem ref="startupItem" :item="startup" ></StartupItem>
+          <StartupItem ref="startupItem" :item="startup"></StartupItem>
         </v-template>
       </ListView>
 
-      <ActivityIndicator :visibility="isBusy ? 'visible' : 'collapsed'" width="100%" height="10%" :busy="isBusy"></ActivityIndicator>
-
-		</StackLayout>
-	</Page>
+      <ActivityIndicator
+        :visibility="isBusy ? 'visible' : 'collapsed'"
+        width="100%"
+        height="10%"
+        :busy="isBusy"
+      ></ActivityIndicator>
+    </StackLayout>
+  </Page>
 </template>
 
 <script>
-import Login from '@/components/Login'
-import IndustriesList from './IndustriesList.vue'
-import StartupItem from '@/components/Startups/StartupItem.vue'
-import StartupView from '@/components/Startups/StartupView.vue'
+import Login from "@/components/Login";
+import IndustriesList from "./IndustriesList.vue";
+import StartupItem from "@/components/Startups/StartupItem.vue";
+import StartupView from "@/components/Startups/StartupView.vue";
 
-import { mapGetters } from 'vuex'
-import { mapActions } from 'vuex'
+import { mapGetters } from "vuex";
+import { mapActions } from "vuex";
 
 export default {
-  props: ['industry'],
+  props: ["industry"],
 
   components: {
     StartupItem
   },
-  
+
   computed: {
     ...mapGetters({
-      itemList: 'itemList',
-      isLoading: 'isProcessing'
+      itemList: "itemList",
+      isLoading: "isProcessing"
     }),
     startups: function() {
-      return this.itemList
-    },
-  },
-
-	data() {
-    return {
-      isBusy: true,
+      return this.itemList;
     }
   },
 
-	mounted() {
+  data() {
+    return {
+      isBusy: true
+    };
   },
 
+  mounted() {},
+
   methods: {
-    ...mapActions([
-      'loadItems',
-    ]),
+    ...mapActions(["loadItems"]),
 
     goBack() {
       this.$navigateBack();
     },
 
     load() {
-      this
-        .loadItems({searchText: this.industry.name, byType: 1})
+      this.loadItems({ searchText: this.industry.name, byType: 1 })
         .then(() => {
-          this.isBusy = false
+          this.isBusy = false;
         })
         .catch(error => {
-          console.error(error)
+          console.error(error);
           alert("An error occurred loading your Startup list.");
-        })
+        });
     },
 
     logout: function() {
-      this.$navigateTo(Login)
+      this.$navigateTo(Login);
     },
 
     showMenu() {
@@ -89,7 +93,7 @@ export default {
         message: "¿Deseas salir?",
         actions: ["Log Off"],
         cancelButtonText: "Cancelar"
-      }).then((result) => {
+      }).then(result => {
         if (result === "Log Off") {
           this.logout();
         }
@@ -97,8 +101,8 @@ export default {
     },
 
     onItemTap({ item }) {
-      this.$navigateTo(StartupView, { props: { item: item } } )
-    },
+      this.$navigateTo(StartupView, { props: { item: item } });
+    }
   }
-}
+};
 </script>
